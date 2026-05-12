1ae59f741ec220f9849f901e4fca5f2437acaaf9
Marco Novas <marco.nicolas.novas@gmail.com>
2026-05-12 00:36:52 -0300

    router

---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+function About() {
+  return (
+    <div>
+      <h1>Sobre la aplicación</h1>
+
+      <p>
+        Esta aplicación fue desarrollada con React como práctica de
+        desarrollo frontend.
+      </p>
+
+      <p>
+        Permite crear, editar, completar y eliminar tareas,
+        además de sincronizar los datos con una API.
+      </p>
+
+      <p>
+        Tecnologías utilizadas:
+      </p>
+
+      <ul>
+        <li>React</li>
+        <li>React Hooks</li>
+        <li>React Router</li>
+        <li>Fetch API</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
